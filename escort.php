6ce36bcdc594ccc0d052db3062702c4284c54dcc
roysesdras <roysesdras49@gmail.com>
2024-03-16 11:53:05 +0100

    saturday 16

--- escort.php
+++ escort.php
@@ -15,6 +15,7 @@
 
     <!-- bootstrap import -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     
     <link rel="canonical" href="...">
 
@@ -35,117 +36,125 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-6">
-                <img src="PHOTO/ALICE/1 (1).jpg" class="w-100" alt="alice" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Alice</p>
+               <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"> <img src="PHOTO/ALICE/1 (1).jpg" class="w-100" alt="alice" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Alice</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/AMARA/1 (1).jpg" class="w-100" alt="amara" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Amara</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"> <img src="PHOTO/AMARA/1 (1).jpg" class="w-100" alt="amara" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Amara</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/ANGELE/1 (2).jpg" class="w-100" alt="angele" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Angele</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/ANGELE/1 (2).jpg" class="w-100" alt="angele" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Angele</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/BIANCA/1 (1).jpg" class="w-100" alt="bianca" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Bianca</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/BIANCA/1 (1).jpg" class="w-100" alt="bianca" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Bianca</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/CARLA/1 (1).jpg" class="w-100" alt="carla" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Carla</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/CARLA/1 (1).jpg" class="w-100" alt="carla" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Carla</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/CASSIE/1 (1).jpg" class="w-100" alt="cassie" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Cassie</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/CASSIE/1 (1).jpg" class="w-100" alt="cassie" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Cassie</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/DORIS/1 (1).jpg" class="w-100" alt="doris" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Doris</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/DORIS/1 (1).jpg" class="w-100" alt="doris" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Doris</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/GUILLA/1 (1).jpg" class="w-100" alt="guilla" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Guilla</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/GUILLA/1 (1).jpg" class="w-100" alt="guilla" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Guilla</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/IRIS/1 (1).jpg" class="w-100" alt="iris" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Iris</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/IRIS/1 (1).jpg" class="w-100" alt="iris" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Iris</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/ISABELLE/1 (12).jpg" class="w-100" alt="isabelle" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Isabelle</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/ISABELLE/1 (12).jpg" class="w-100" alt="isabelle" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Isabelle</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/JESSICA/1 (2).jpg" class="w-100" alt="jessica" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Jessica</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/JESSICA/1 (2).jpg" class="w-100" alt="jessica" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Jessica</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/KATE/1 (1).jpg" class="w-100" alt="kate" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Kate</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/KATE/1 (1).jpg" class="w-100" alt="kate" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Kate</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/KIARA/1 (1).jpg" class="w-100" alt="kiara" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Kiara</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/KIARA/1 (1).jpg" class="w-100" alt="kiara" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Kiara</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/LARISSA/1 (1).jpg" class="w-100" alt="larissa" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Larissa</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/LARISSA/1 (1).jpg" class="w-100" alt="larissa" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Larissa</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/LEONORE/1 (1).jpg" class="w-100" alt="leonore" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Leonore</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/LEONORE/1 (1).jpg" class="w-100" alt="leonore" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Leonore</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/LETICIA/1 (1).jpg" class="w-100" alt="leticia" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Leticia</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/LETICIA/1 (1).jpg" class="w-100" alt="leticia" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Leticia</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/LUCIE/1 (1).jpg" class="w-100" alt="lucie" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Lucie</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/LUCIE/1 (1).jpg" class="w-100" alt="lucie" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Lucie</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/MIA/1 (1).jpg" class="w-100" alt="mia" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Mia</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/MIA/1 (1).jpg" class="w-100" alt="mia" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Mia</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/SANDRINE/1 (4).jpg" class="w-100" alt="sandrine" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Sandrine</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/SANDRINE/1 (4).jpg" class="w-100" alt="sandrine" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Sandrine</p></a>
             </div>
 
             <div class="col-md-3 col-6">
-                <img src="PHOTO/URSULA/1 (1).jpg" class="w-100" alt="ursula" id="photo">
-                <p class="bg-danger-subtle opacity-75 p-1">Ursula</p>
+            <a href="#"data-toggle="tooltip" title="mehr Fotos? Fragen"><img src="PHOTO/URSULA/1 (1).jpg" class="w-100" alt="ursula" id="photo">
+                <p class="bg-danger-subtle opacity-75 p-1">Ursula</p></a>
             </div>
 
         </div>
     </div>
 
+    <div class="reservation">
+        <?php include('inclusion/reservierung.php'); ?>
+    </div>
         
-    
-    
-    
-    <footer class="p-3 bg-danger-subtle opacity-75 text-center">
-        &copy; <strong><span>Erotische Lust</span></strong>. Alle Rechte vorbehalten
-    </footer>
+  
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<footer class="p-3 bg-danger-subtle opacity-75 text-center">
+    &copy; <strong><span>Erotische Lust</span></strong>. Alle Rechte vorbehalten
+</footer>
+
+<script>
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
---
+++ inclusion/reservierung.php
@@ -0,0 +1,136 @@
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                    <h2 class="text-center pt-5 mb-2" id="souscription">Anmeldeformular</h2>
+                    <p class="text-center">Füllen Sie das untenstehende Anmeldeformular aus, um von unseren außergewöhnlichen Dienstleistungen zu profitieren. <br>Wir freuen uns über Ihren Besuch und bieten Ihnen maßgeschneiderte Lösungen, die Ihren finanziellen Bedürfnissen entsprechen. </p>
+                        <form action="mail_souscription.php" method="post" class="mb-4">
+                            <!-- Nom -->
+                            <div class="mb-3">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="nom" required>
+                            </div>
+
+                            <!-- Prenom -->
+                            <div class="mb-3">
+                                <label>Vorname</label>
+                                <input type="text" class="form-control" name="prenom" required>
+                            </div>
+
+                            <!-- age -->
+                            <div class="mb-3">
+                                <label>Alter</label>
+                                <input type="number" class="form-control" name="age" min="2" required>
+                            </div>
+
+                            <!-- Ville -->
+                            <div class="mb-3">
+                                <label>Stadt</label>
+                                <input type="text" class="form-control" name="city" required>
+                            </div>
+
+                            <!-- Code postal -->
+                            <div class="mb-3">
+                                <label>Postleitzahl</label>
+                                <input type="text" class="form-control" name="code_postal" title="Entrez un code postal valide" required>
+                            </div>
+
+                            <!-- Adresse complète -->
+                            <div class="mb-3">
+                                <label>Adresse</label>
+                                <input type="text" class="form-control" name="adresse" required placeholder="Bitte geben Sie Ihre vollständige Adresse an.">
+                            </div>
+
+                            <!-- Profession -->
+                            <div class="mb-3">
+                                <label>Beruf</label>
+                                <input type="text" class="form-control" name="profession" placeholder="Was machen Sie beruflich?" required>
+                            </div>
+
+                            <!-- E-mail -->
+                            <div class="mb-4">
+                                <label>E-Mail</label>
+                                <input type="email" class="form-control" name="email" placeholder="test@example.com" required>
+                            </div>
+
+                            <!-- Numero de telephone/whatsap -->
+                            <div class="mb-4">
+                                <label>Telefonnummer/WhatsApp</label>
+                                <input type="tel" class="form-control" name="phone" max="15" placeholder="(+49) xxxxxx" required>
+                            </div>
+
+                            <!-- Option d'escorte choisie -->
+                            <select class="form-select mb-3" name="option_escorte" required>
+                                <option selected>Wählen Sie eine Escort</option>
+                                <option value="alice">Alice</option>
+                                <option value="amara">Amara</option>
+                                <option value="angele">Angele</option>
+                                <option value="bianca">Bianca</option>
+                                <option value="carla">Carla</option>
+                                <option value="cassie">Cassie</option>
+                                <option value="doris">Doris</option>
+                                <option value="guilla">Guilla</option>
+                                <option value="iris">Iris</option>
+                                <option value="isabelle">Isabelle</option>
+                                <option value="jessica">Jessica</option>
+                                <option value="kate">Kate</option>
+                                <option value="kiara">Kiara</option>
+                                <option value="larissa">Larissa</option>
+                                <option value="leonore">Leonore</option>
+                                <option value="leticia">Leticia</option>
+                                <option value="lucie">Lucie</option>
+                                <option value="mia">Mia</option>
+                                <option value="sandrine">Sandrine</option>
+                                <option value="ursula">Ursula</option>
+                            </select>
+
+                            <!-- Date et heure du rendez-vous -->
+                            <div class="mb-4">
+                                <label>Datum und Uhrzeit des Termins festlegen</label>
+                                <input type="datetime-local" class="form-control" name="date_time" min="2024-01-01T00:00" max="2030-01-01T00:00"  required>
+                            </div>
+
+                            <!-- lieu du rendez-vous -->
+                            <select class="form-select mb-4" name="lieu_rencontre" required>
+                                <option selected>Wählen Treffpunkt</option>
+                                <option value="hotel">Hotels</option>
+                                <option value="domi_privE">Privathaus</option>
+                                <option value="apprt_privE">Private Wohnungen</option>
+                                <option value="domi_escorte">Zuhause der Eskorte</option>
+                            </select>
+
+                            <!-- Durée de rencontre -->
+                            <select class="form-select mb-1" name="lieu_rencontre" required>
+                                <option selected>Wählen Sie die Dauer des Treffens</option>
+                                <option value="2hr">2 Stunden > 392€</option>
+                                <option value="3hr">3 Stunden > 540€</option>
+                                <option value="6hr_jour">6 Stunden (1/2 Tage) > 850€</option>
+                                <option value="6hr_nuit">6 Stunden (1/2 Nächte) > 996€</option>
+                                <option value="12hr_nuit">12 Stunden (nachts) > 1250€</option>
+                                <option value="1jr_12hr">1 Tag (24 Stunden) > 1775€</option>
+                                <option value="1_semain">1 Woche (7 Tage) > 4483€</option>
+                            </select>
+
+                            <div class="court_text mb-3">
+                                <label for="">
+                                    Jede weitere Stunde wird mit 130€ berechnet und jeder weitere Tag wird mit 992€berechnet
+                                </label>
+                            </div>
+
+                            <div class="form-check mb-4">
+                                <input class="form-check-input" type="checkbox" id="gridCheck" required>
+                                <label class="form-check-label" for="gridCheck">
+                                    Mit dem Absenden dieses Formulars Stimmen Sie unseren <a href="#">Allgemeinen Nutzungsbedingungen</a> sowie unseren <a href="#">zahlungsrichtlinien</a>zu.
+                                </label>
+                            </div>
+
+      
+                            <input type="submit" name="submit" class="btn btn-primary" value="Ich abonniere">
+                        </form>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+            </div>
+            
+            
